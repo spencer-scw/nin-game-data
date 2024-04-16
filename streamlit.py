1908d11f41665ec22227f4d52cc720f16c510db5
spencer-scw <spencer.scw@gmail.com>
2024-04-16 10:27:17 -0600

    started streamlit app

---
+++ streamlit.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import datetime as dt
+from dateutil import parser;
+
+nin_titles = pd.read_csv("Best_Selling_Nintendo_Titles.csv")
+
+nin_titles["Release_Date"] = nin_titles["Release_Date"].apply(parser.parse)
+
+st.line_chart(nin_titles.style, x = "Release_Date", y = "Sales")
+
+year = st.slider("Bestseller by year", 1983, 2023)
+year = st.number_input("Bestseller by year (1983 - 2023)")
+
+nin_titles["Year"] = nin_titles["Release_Date"].apply(lambda x: x.year)
+
+st.write(nin_titles.loc[nin_titles.groupby(["Year"])["Sales"].idxmax()[year]]["Game"])
